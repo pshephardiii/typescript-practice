c364c4d82073d8d18bbe2a6d7528a1da5a373eb1
Paul Shephard <paulshephard598@gmail.com>
2024-07-17 15:37:10 -0400

    initial commit

--- dist/index.377278e2.js
+++ dist/index.377278e2.js
@@ -1,3 +1,27 @@
-console.log("Hello World From typeScript");
+console.log("Hello World From typeScript") // EXERCISE: strictly typed nested objects
+ // type Caterer = {
+ //     name: string
+ //     address: string
+ //     phone: number
+ // }
+ // type Airplane = {
+ //     model: string
+ //     flightNumber: string
+ //     timeOfDeparture: Date
+ //     timeOfArrival: Date
+ //     caterer: Caterer
+ // }
+ // const plane1: Airplane = {
+ //     model: "Airbus A380",
+ //     flightNumber: "A2201",
+ //     timeOfDeparture: new Date(),
+ //     timeOfArrival: new Date(),
+ //     caterer: {
+ //         name: "Special Food Ltd",
+ //         address: "484, Some Street, New York",
+ //         phone: 1452125,
+ //     }
+ // }
+;
 
 //# sourceMappingURL=index.377278e2.js.map
---
+++ src/index.js
@@ -0,0 +1,3 @@
+console.log("Hello World From typeScript");
+var bigInt = 1234456n;
+console.log(typeof bigInt);
